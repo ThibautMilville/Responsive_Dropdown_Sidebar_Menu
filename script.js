3087930fb23df0dd59bbb7381434ffc588c9bb57
ThibautMilville <tmilville.pro@gmail.com>
2023-05-05 16:36:35 +0200

    bug resolution

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ let arrow = document.querySelectorAll('.arrow');
 for (var i = 0; i < arrow.length; i++) {
   arrow[i].addEventListener('click', (e) => {
     let arrowParent = e.target.parentElement.parentElement;
-    console.log(arrowParent);
     arrowParent.classList.toggle('showMenu');
   });
 }
+
+let sidebar = document.querySelector('.sidebar');
+let sidebarBtn = document.querySelector('.bx-menu');
+
+sidebarBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('close');
+});
